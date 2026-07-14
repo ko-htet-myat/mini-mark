3dc448276140131c9cd3d0d3da228358afd46a39
ko-htet-myat <htetmyataung232002@gmail.com>
2026-07-15 00:50:35 +0630

    feat: add better-auth and prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div className=" h-screen w-full grid place-items-center">
-      <h2 className=" text-2xl">Hello From Myanmar (မင်္ဂလာပါ)</h2>
-    </div>
+    <main className="flex items-center justify-center h-screen bg-neutral-950 text-white">
+      <div>
+        <h2 className=" text-2xl">Hello From Myanmar (မင်္ဂလာပါ)</h2>
+        <div className="flex gap-4 mt-5 justify-center">
+          <button
+            onClick={() => router.push("/sign-up")}
+            className="bg-white text-black font-medium px-6 py-2 rounded-md hover:bg-gray-200"
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={() => router.push("/sign-in")}
+            className="border border-white text-white font-medium px-6 py-2 rounded-md hover:bg-neutral-800"
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
